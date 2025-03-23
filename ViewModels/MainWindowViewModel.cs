7ba1361cdb623d999ad24ce41573aef203b486e6
Boti21 <botibencsik@gmail.com>
2025-03-23 15:52:10 +0100

    Added naudio basic file playing

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,38 @@
-﻿namespace Haptics_GUI.ViewModels;
+﻿using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Haptics_GUI.Models;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+
+namespace Haptics_GUI.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    [ObservableProperty] public string filePath;
+    private AudioFileReader audioFile;
+    private WasapiOut audioOut;
+
+
+    [RelayCommand]
+    public void PlayFile()
+    {
+        audioFile = new AudioFileReader(filePath);
+        
+        audioOut = new WasapiOut(AudioClientShareMode.Exclusive, false, 100);
+        
+        audioOut.Init(audioFile);
+        audioOut.Play();
+    }
+
+    [RelayCommand]
+    public void StopFile()
+    {
+        audioOut.Stop();
+        audioOut.Dispose();
+    }
+    public MainWindowViewModel()
+    {
+        filePath = string.Empty;
+    }
 }

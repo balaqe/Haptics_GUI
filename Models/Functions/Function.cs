4e0926d632c273c937b78fc72752015c89f0b036
Balage <basza22@student.sdu.dk>
2025-04-01 13:55:21 +0200

    Refactor sine gen

--- Models/Functions/Function.cs
+++ Models/Functions/Function.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Haptics_GUI.Models.Functions;
 
 public abstract class Function
@@ -9,6 +11,53 @@ public abstract class Function
     public uint samplingRate;
     public uint bitDepth;
     public byte[] waveform;
+    public abstract double Func(double inFreq, uint inSamplingRate, uint i);
 
-    public abstract void Generator();
+    public Function(double inFreq, double inDur, uint inChannel, uint inStart, uint inSamplingRate, uint inBitDepth)
+    {
+        freq = inFreq;
+        dur = inDur;
+        channel = inChannel;
+        start = inStart;
+        samplingRate = inSamplingRate;
+        bitDepth = inBitDepth;
+        Generator();
+    }
+
+    public void Generator()
+    {
+        uint noOfSamples = (uint)((double)samplingRate * (dur + start));
+        uint samplesToStart = (uint)((double)samplingRate * start);
+        uint byteLength = noOfSamples * bitDepth / 8;
+        // uint bytesToStart = samplesToStart * bitDepth / 8;
+        
+        waveform = new byte[byteLength];
+
+        for (uint i = samplesToStart; i < noOfSamples; i++)
+        {
+            double sampleValue = Func(freq, samplingRate, i-samplesToStart);
+
+            byte[] valueBytes;
+            switch (bitDepth)
+            {
+                case 8:
+                    valueBytes = new byte[] { (byte)(sampleValue * 127 + 128) }; // 8-bit audio is unsigned
+                    break;
+                
+                case 16:
+                    valueBytes = BitConverter.GetBytes((short)(sampleValue * short.MaxValue));
+                    break;
+                
+                default:
+                    valueBytes = BitConverter.GetBytes((uint)(sampleValue * uint.MaxValue));
+                    break;
+            }
+            
+            uint byteIndex = i * (bitDepth / 8);
+            for (int j = 0; j < bitDepth/8; j += 1)
+            {
+                waveform[byteIndex + j] = valueBytes[j];
+            }
+        }
+    }
 }
--- Models/Functions/Sine.cs
+++ Models/Functions/Sine.cs
@@ -5,54 +5,12 @@ namespace Haptics_GUI.Models.Functions;
 public class Sine : Function
 {
     public Sine(double inFreq, double inDur, uint inChannel, uint inStart, uint inSamplingRate, uint inBitDepth)
+    : base(inFreq, inDur, inChannel, inStart, inSamplingRate, inBitDepth)
+    {}
+
+    public override double Func(double inFreq, uint inSamplingRate, uint i)
     {
-        freq = inFreq;
-        dur = inDur;
-        channel = inChannel;
-        start = inStart;
-        samplingRate = inSamplingRate;
-        bitDepth = inBitDepth;
-        Generator();
-    }
-
-    public override void Generator()
-    {
-        uint noOfSamples = (uint)((double)samplingRate * (dur + start));
-        uint samplesToStart = (uint)((double)samplingRate * start);
-        uint byteLength = noOfSamples * bitDepth / 8;
-        // uint bytesToStart = samplesToStart * bitDepth / 8;
-        
-        waveform = new byte[byteLength];
-
-        for (uint i = samplesToStart; i < noOfSamples; i++)
-        {
-            double omega = 2*Math.PI*freq/(double)samplingRate;
-            double sampleValue = Math.Sin(omega * (i - samplesToStart));
-
-            byte[] valueBytes;
-            switch (bitDepth)
-            {
-                case 8:
-                    valueBytes = new byte[] { (byte)(sampleValue * 127 + 128) }; // 8-bit audio is unsigned
-                    break;
-                
-                case 16:
-                    valueBytes = BitConverter.GetBytes((short)(sampleValue * short.MaxValue));
-                    break;
-                
-                default:
-                    valueBytes = BitConverter.GetBytes((uint)(sampleValue * uint.MaxValue));
-                    break;
-            }
-            
-            uint byteIndex = i * (bitDepth / 8);
-            for (int j = 0; j < bitDepth/8; j += 1)
-            {
-                // Console.WriteLine($"waveForm[byteIndex + j] {byteIndex + j}");
-                // Console.WriteLine($"valueBytes[j] {j}");
-                waveform[byteIndex + j] = valueBytes[j];
-            }
-        }
-        
+        double omega = 2*Math.PI*inFreq/(double)inSamplingRate;
+        return Math.Sin(omega * i);
     }
 }

cd10d74e667d6ac4d92a89396c74d0db9a70051e
Boti21 <botibencsik@gmail.com>
2025-04-24 17:33:30 +0200

    nyi nyi nyiii Boti vagyok

--- Models/Functions/Sine.cs
+++ Models/Functions/Sine.cs
@@ -10,7 +10,7 @@ public class Sine : Function
 
     public override double Func(double inFreq, int inSamplingRate, int i)
     {
-        double omega = 2*Math.PI*inFreq/(double)inSamplingRate; // Calculate period
+        double omega = 2*Math.PI*inFreq/(double)inSamplingRate; // Calculate period: 2*pi / omega
         return Math.Sin(omega * i);
     }
 }
